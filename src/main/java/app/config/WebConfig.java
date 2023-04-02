e40d5202dc4eaa138ae601ceec2e591c60bce432
Zach Auten <zachauten@gmail.com>
2023-04-01 20:37:38 -0400

    Add CommonsRequestLoggingFilter config

--- src/main/java/app/config/WebConfig.java
+++ src/main/java/app/config/WebConfig.java
@@ -2,6 +2,7 @@ package app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,6 +17,17 @@ public class WebConfig implements WebMvcConfigurer {
 
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(new ResponseTimeInterceptor());
+  }
+
+  @Bean
+  public CommonsRequestLoggingFilter logFilter() {
+      var filter
+        = new CommonsRequestLoggingFilter();
+      filter.setIncludeQueryString(true);
+      filter.setIncludePayload(true);
+      filter.setMaxPayloadLength(10000);
+      filter.setIncludeHeaders(false);
+      return filter;
   }
 
   // @Bean
