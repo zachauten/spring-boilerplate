05a98ecdf25743e0a0369609506de5267a38b5e1
Zach Auten <zachauten@gmail.com>
2023-10-03 21:56:11 -0400

    Remove Either, repository classes

--- src/main/java/app/repositories/WidgetRepository.java
+++
@@ -1,9 +0,0 @@
-package app.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import app.repositories.entities.Widget;
-
-public interface WidgetRepository extends CrudRepository<Widget, Long> {
-  
-}
--- src/main/java/app/repositories/entities/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package app.repositories.entities;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<Person, Long> {
-  
-}
