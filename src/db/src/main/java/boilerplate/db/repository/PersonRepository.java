cb0ead8a01389e38601ce2011e278fd1ebade1cd
Zach Auten <zauten@bandwidth.com>
2025-10-20 14:01:31 -0400

    fix import

--- src/db/src/main/java/boilerplate/db/repository/PersonRepository.java
+++ src/db/src/main/java/boilerplate/db/repository/PersonRepository.java
@@ -2,6 +2,8 @@ package boilerplate.db.repository;
 
 import boilerplate.db.entities.Person;
 import java.util.List;
+
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
